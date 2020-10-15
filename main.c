5187a24254e1f8084e9394ae8bfd7b482ec6277c
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-10-15 15:46:59 +0900

    02

--- main.c
+++ main.c
@@ -4,15 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int flag=1;
-	int y;
-	
-	while(flag!=0){
+	int i;
+	int temp=1;
+	for(i=0;i<5;i++){
 		
-		y=3;
-		flag=0;
-	}
-	y=4;
-	
+		printf("temp=%d\n",temp);
+		temp++;
+	} 
 	return 0;
+	
 }
